688e76df7364ab0f65d1c2e378964a616a631936
Will Nel <will.birley.nel@outlook.com>
2021-08-24 15:26:52 +0100

    WN:Calculator files created

--- src/main/java/HelloWorld/HelloWorld.java
+++ src/main/java/HelloWorld/HelloWorld.java
@@ -3,8 +3,18 @@ package HelloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hello World!");
+		String message = "Hello World!";
+		System.out.println(message);
+		printMessage();
+		
+		
+	}
+	
+	public static void printMessage() {
+		String message = "Hello Will";
+		System.out.println(message);
 	}
 
 }
