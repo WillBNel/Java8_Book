263a7d01e04ddb49c77edb9ef99cfaa9324882ad
Will Nel <will.birley.nel@outlook.com>
2021-08-24 16:54:32 +0100

    WN:CheckString files created

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -3,8 +3,31 @@ package Calculator;
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(addTwo(8084d, 1540d));
+		System.out.println(mulTwo(145d, 56d));
+		System.out.println(minTwo(445546d, 1546544d));
+		divTwo(6d, 3d);
 	}
 
+	public static double addTwo(double numb1, double numb2) {
+		return numb1 + numb2;
+	}
+
+	public static double mulTwo(double numb1, double numb2) {
+		return numb1 * numb2;
+	}
+
+	public static double minTwo(double numb1, double numb2) {
+		return numb1 - numb2;
+	}
+
+	public static void divTwo(double numb1, double numb2) {
+		if (numb1 < numb2) {
+			System.out.println("The devision cannot be perfomed");
+
+		} else {
+			System.out.println(numb1 / numb2);
+		}
+
+	}
 }
---
+++ src/main/java/CheckString/CheckString.java
@@ -0,0 +1,10 @@
+package CheckString;
+
+public class CheckString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
